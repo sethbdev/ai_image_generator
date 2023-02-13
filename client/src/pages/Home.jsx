787476f6efe7b0cd58e677ae9bdef32d82cfc2aa
Seth <seth.bbrendle@gmail.com>
2023-02-13 11:48:47 -0500

    changed conditional rendering to temporarily return array in RenderCards in Home.jsx so the mapping does not error. change once functionality added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -44,12 +44,12 @@ const Home = () => {
             <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
               { searchText ? (
                 <RenderCards 
-                  data="searchedResults"
+                  data={[]}
                   title="No Posts found"
                 />
               ) : (
                 <RenderCards
-                  data="allPosts"
+                  data={[]}
                   title="No Posts found"
                 />
                 )
