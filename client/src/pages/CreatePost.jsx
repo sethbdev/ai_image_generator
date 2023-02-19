039d8ed178387304bd847bda59abc97802aff2f6
Seth <seth.bbrendle@gmail.com>
2023-02-19 14:23:52 -0500

    conditional rendering button for sharing the generated image  in CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -91,6 +91,16 @@ const CreatePost = () => {
                 {generatingImg ? 'Generating ...' : 'Generate'}
               </button>
         </div>
+
+        <div className='mt-10'>
+          <p className='mt-2 text-[#666e75] text-[14px]'>Once you have generated the image you prefer, you can share it with the community</p>
+          <button
+            type='submit'
+            className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+          >
+            {loading ? 'Sharing ...' : 'Share with the community'}
+          </button>
+        </div>
       </form>
 
     </section>
