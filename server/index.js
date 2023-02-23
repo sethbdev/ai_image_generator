b2ca87f25677504be6cb035c53e23d8047016a1e
Seth <seth.bbrendle@gmail.com>
2023-02-23 08:27:58 -0500

    created routes, dalleRoutes.js, postRoutes.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './mongoDB/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 
 //pulls environment variables from dotenv file
 dotenv.config();
@@ -10,6 +12,9 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalle);
 
 app.get('/', async (req, res) => {
     res.send('Hello from DALL-E!');
