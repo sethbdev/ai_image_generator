5098e17ce84e5ce93ae269d5653fe8ad2fc2804f
Seth <seth.bbrendle@gmail.com>
2023-02-26 12:36:40 -0500

    post creation functionality addedto server/routes/postRoutes.js

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -14,4 +14,29 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+//GET ALL POSTS
+router.route('/').get(async (req, res) => {
+
+});
+
+//CREATE A POST
+router.route('/').post(async (req, res) => {
+    try {
+        const { name, prompt, photo } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
+    
+    //will create new post in database
+    const newPost = await Post.create({
+        name,
+        prompt,
+        // photo: photoUrl.secure_url,
+        photo: photoUrl.url,
+    })
+
+    res.status(201).json({ success: true, data: newPost });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error })
+    }
+});
+
 export default router;
